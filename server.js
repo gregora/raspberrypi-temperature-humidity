e3de795e230322b7e0228cd86fa46bffd784a6cc
Gregor A <gregor.alic@gimb.org>
2021-01-18 23:31:07 +0100

    Minor fix

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function readHistory(){
 
   con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
+    con.query("SELECT * FROM data", function (err, result, fields) {
       if (err) throw err;
       return result;
     });
